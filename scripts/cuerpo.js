5ec5a448d88defa0276fdfea2fded25ff2b98506
David-Diez <59050043+David-Diez@users.noreply.github.com>
2020-01-04 18:19:50 +0100

    Add files via upload

---
+++ scripts/cuerpo.js
@@ -0,0 +1,44 @@
+
+var fecha = getDate();
+var m= fecha.getMonth();
+
+function cambiofondo(){
+	/**if(m=0)**/
+		document.body.style.backgroundImage = "url('../images/winter.jpg')" ;
+	/**
+	if(m=1){
+		document.body.style.backgroundImage = "url('../images/winter.jpg')" ;
+	}
+	if(m=2){
+		document.body.style.backgroundImage = "url('../images/spring.jpg')" ;
+	}
+	if(m=3){
+		document.body.style.backgroundImage = "url('../images/spring.jpg')" ;
+	}
+	if(m=4){
+		document.body.style.backgroundImage = "url('../images/spring.jpg')" ;
+	}
+	if(m=5){
+		document.body.style.backgroundImage = "url('../images/summer.jpg')" ;
+	}
+	if(m=6){
+		document.body.style.backgroundImage = "url('../images/summer.jpg')" ;
+	}
+	if(m=7){
+		document.body.style.backgroundImage = "url('../images/summer.jpg')" ;
+	}
+	if(m=8){
+		document.body.style.backgroundImage = "url('../images/summer.jpg')" ;
+	}
+	if(m=9){
+		document.body.style.backgroundImage = "url('../images/autumn.jpg')" ;
+	}
+	if(m=10){
+		document.body.style.backgroundImage = "url('../images/autumn.jpg')" ;
+	}
+	if(m=11){
+		document.body.style.backgroundImage = "url('../images/winter.jpg')" ;
+	}
+	
+	
+}
